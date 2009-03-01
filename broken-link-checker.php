49069b028e6d5631e9a2cdab69bb4f71a1c11355
whiteshadow <whiteshadow@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2009-03-01 18:53:39 +0000

    * (Hopefully) fix join() failure when Snoopy doesn't return any HTTP headers.
    git-svn-id: https://plugins.svn.wordpress.org/broken-link-checker/tags/0.4.13@98711 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- broken-link-checker.php
+++ broken-link-checker.php
@@ -3,7 +3,7 @@
 Plugin Name: Broken Link Checker
 Plugin URI: http://w-shadow.com/blog/2007/08/05/broken-link-checker-for-wordpress/
 Description: Checks your posts for broken links and missing images and notifies you on the dashboard if any are found.
-Version: 0.4.12
+Version: 0.4.13
 Author: Janis Elsts
 Author URI: http://w-shadow.com/blog/
 */
@@ -23,7 +23,7 @@ class ws_broken_link_checker {
     var $options_name='wsblc_options';
     var $postdata_name;
     var $linkdata_name;
-    var $version='0.4.12';
+    var $version='0.4.13';
     var $myfile='';
     var $myfolder='';
     var $mybasename='';
@@ -320,7 +320,8 @@ class ws_broken_link_checker {
             if ($snoopy->timed_out)
                 $result['log'] .= "Request timed out.\n";
 
-            $result['log'] .= join("", $snoopy->headers)."\n"; //those headers already contain newlines
+			if (is_array($snoopy->headers))
+            	$result['log'] .= implode("", $snoopy->headers)."\n"; //those headers already contain newlines
 
             //$result['log'] .= print_r($snoopy, true);
 
