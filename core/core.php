d591609d62478b929c388cf8193a117a041307a3
whiteshadow <whiteshadow@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2010-12-24 09:54:55 +0000

    * Moved the Settings page sidebar into a separate file. * Added an ad for MaxCDN. This only shows up if the user has at least 10 published posts (may require tweaking).
    git-svn-id: https://plugins.svn.wordpress.org/broken-link-checker/trunk@325876 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- core/core.php
+++ core/core.php
@@ -607,47 +607,7 @@ class wsBrokenLinkChecker {
 		
         <div id="blc-sidebar">
 			<div class="metabox-holder">
-			
-				<div id="donate" class="postbox">
-					<h3 class="hndle"><?php _e('Donate $10, $20 or $50!', 'broken-link-checker'); ?></h3>
-					<div class="inside">
-						<p><?php
-						_e('If you like this plugin, please donate to support development and maintenance!', 'broken-link-checker');							
-						?></p>
-						
-						<form style="text-align: center;" action="https://www.paypal.com/cgi-bin/webscr" method="post">
-							<input type="hidden" name="cmd" value="_donations">
-							<input type="hidden" name="business" value="G3GGNXHBSHKYC">
-							<input type="hidden" name="lc" value="US">
-							<input type="hidden" name="item_name" value="Broken Link Checker">
-							<input type="hidden" name="no_note" value="1">
-							<input type="hidden" name="no_shipping" value="1">
-							<input type="hidden" name="currency_code" value="USD">
-							<input type="hidden" name="bn" value="PP-DonationsBF:btn_donateCC_LG.gif:NonHosted">
-							
-							<input type="hidden" name="return" value="<?php 
-								echo esc_attr(admin_url('options-general.php?page=link-checker-settings&donated=1')); 
-							?>" />
-							<input type="hidden" name="cbt" value="<?php 
-								echo esc_attr(__('Return to WordPress Dashboard', 'broken-link-checker')); 
-							?>" />
-							<input type="hidden" name="cancel_return" value="<?php 
-								echo esc_attr(admin_url('options-general.php?page=link-checker-settings&donation_canceled=1')); 
-							?>" />
-							
-							<input type="image" src="https://www.sandbox.paypal.com/en_US/GB/i/btn/btn_donateCC_LG.gif" border="0" name="submit" alt="PayPal - The safer, easier way to pay online.">
-						</form>
-					</div>					
-				</div>
-				
-				<div id="advertising" class="postbox">
-					<h3 class="hndle">Like this plugin?</h3>
-					<div class="inside">
-						<p>You might also like:</p>
-						<p>(put fancy ads here)</p>
-					</div>					
-				</div>
-				
+				<?php include dirname(blc_get_plugin_file()) . '/includes/admin/sidebar.php'; ?>
 			</div>
 		</div>
 		
---
+++ includes/admin/sidebar.php
@@ -0,0 +1,54 @@
+<div id="donate" class="postbox">
+	<h3 class="hndle"><?php _e('Donate $10, $20 or $50!', 'broken-link-checker'); ?></h3>
+	<div class="inside">
+		<p><?php
+		_e('If you like this plugin, please donate to support development and maintenance!', 'broken-link-checker');							
+		?></p>
+		
+		<form style="text-align: center;" action="https://www.paypal.com/cgi-bin/webscr" method="post">
+			<input type="hidden" name="cmd" value="_donations">
+			<input type="hidden" name="business" value="G3GGNXHBSHKYC">
+			<input type="hidden" name="lc" value="US">
+			<input type="hidden" name="item_name" value="Broken Link Checker">
+			<input type="hidden" name="no_note" value="1">
+			<input type="hidden" name="no_shipping" value="1">
+			<input type="hidden" name="currency_code" value="USD">
+			<input type="hidden" name="bn" value="PP-DonationsBF:btn_donateCC_LG.gif:NonHosted">
+			
+			<input type="hidden" name="return" value="<?php 
+				echo esc_attr(admin_url('options-general.php?page=link-checker-settings&donated=1')); 
+			?>" />
+			<input type="hidden" name="cbt" value="<?php 
+				echo esc_attr(__('Return to WordPress Dashboard', 'broken-link-checker')); 
+			?>" />
+			<input type="hidden" name="cancel_return" value="<?php 
+				echo esc_attr(admin_url('options-general.php?page=link-checker-settings&donation_canceled=1')); 
+			?>" />
+			
+			<input type="image" src="https://www.sandbox.paypal.com/en_US/GB/i/btn/btn_donateCC_LG.gif" border="0" name="submit" alt="PayPal - The safer, easier way to pay online.">
+		</form>
+	</div>					
+</div>
+
+<?php
+$post_counts = wp_count_posts();
+$show_ad = isset($post_counts->publish) && ($post_counts->publish >= 10);
+
+if ( $show_ad ):
+?>
+
+<div id="advertising" class="postbox">
+	<h3 class="hndle"><?php _e('Recommended', 'broken-link-checker'); ?></h3>
+	<div class="inside" style="text-align: center;">
+		<a href="http://www.maxcdn.com/wordpress-cdn.php?type=banner&&affId=102167&&img=c_160x600_maxcdn_simple.gif">
+			<img src="<?php echo esc_attr(plugins_url('images/maxcdn.gif', blc_get_plugin_file())); ?>" border=0>
+		</a>
+		<img src="http://impression.clickinc.com/impressions/servlet/Impression?merchant=70291&&type=impression&&affId=102167&&img=c_160x600_maxcdn_simple.gif" style="display:none" border=0>
+	</div>					
+</div>
+
+<?php
+
+endif;
+
+?>
