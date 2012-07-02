a3689eb82d066686e4a4d95b2a33e0142241c3c9
whiteshadow <whiteshadow@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-07-02 07:32:48 +0000

    Update CSS stylesheet version.
    git-svn-id: https://plugins.svn.wordpress.org/broken-link-checker/trunk@566321 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- core/core.php
+++ core/core.php
@@ -1889,7 +1889,7 @@ class wsBrokenLinkChecker {
 	 * @return void
 	 */
 	function links_page_css(){
-		wp_enqueue_style('blc-links-page', plugins_url('css/links-page.css', $this->loader), array(), '0.9.6' );
+		wp_enqueue_style('blc-links-page', plugins_url('css/links-page.css', $this->loader), array(), '20120702');
 	}
 	
 	/**
