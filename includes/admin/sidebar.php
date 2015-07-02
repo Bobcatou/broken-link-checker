f4fda7775be830e0157d89220459a91f93812da1
whiteshadow <whiteshadow@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-07-02 12:41:01 +0000

    Removed the donation button.
    git-svn-id: https://plugins.svn.wordpress.org/broken-link-checker/trunk@1191230 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- includes/admin/sidebar.php
+++ includes/admin/sidebar.php
@@ -1,38 +1,4 @@
-<!-- Donation button -->
-<div id="donate" class="postbox">
-	<h3 class="hndle"><?php _e('Donate $10, $20 or $50!', 'broken-link-checker'); ?></h3>
-	<div class="inside">
-		<p><?php
-		_e('If you like this plugin, please donate to support development and maintenance!', 'broken-link-checker');							
-		?></p>
-		
-		<form style="text-align: center;" action="https://www.paypal.com/cgi-bin/webscr" method="post">
-			<input type="hidden" name="cmd" value="_donations">
-			<input type="hidden" name="business" value="G3GGNXHBSHKYC">
-			<input type="hidden" name="lc" value="US">
-			<input type="hidden" name="item_name" value="Broken Link Checker">
-			<input type="hidden" name="no_note" value="1">
-			<input type="hidden" name="no_shipping" value="1">
-			<input type="hidden" name="currency_code" value="USD">
-			<input type="hidden" name="bn" value="PP-DonationsBF:btn_donateCC_LG.gif:NonHosted">
-			
-			<input type="hidden" name="rm" value="2">
-			<input type="hidden" name="return" value="<?php 
-				echo esc_attr(admin_url('options-general.php?page=link-checker-settings&donated=1')); 
-			?>" />
-			<input type="hidden" name="cbt" value="<?php 
-				echo esc_attr(__('Return to WordPress Dashboard', 'broken-link-checker')); 
-			?>" />
-			<input type="hidden" name="cancel_return" value="<?php 
-				echo esc_attr(admin_url('options-general.php?page=link-checker-settings&donation_canceled=1')); 
-			?>" />
-			
-			<input type="image" src="https://www.sandbox.paypal.com/en_US/GB/i/btn/btn_donateCC_LG.gif" name="submit" alt="PayPal - The safer, easier way to pay online." style="max-width:170px;height:47px;border:0;">
-		</form>
-	</div>					
-</div>
-
-<!-- Other advertising -->
+<!-- Advertising -->
 <?php
 $configuration = blc_get_configuration();
 if ( !$configuration->get('user_has_donated') ):
